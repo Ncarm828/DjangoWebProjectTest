9f802afd73805d20e4e7da6169422e78a65b1139
Nicholas Carmen <NicholasJCarmen@gmail.com>
2018-11-05 19:14:56 -0500

    Add project files.

---
+++ DjangoWebProjectTest/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Nicks\Source\Repos\DjangoWebProjectTest\DjangoWebProjectTest\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
